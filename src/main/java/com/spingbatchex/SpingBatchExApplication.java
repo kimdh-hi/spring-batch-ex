bf126609103fb5f8a5c7ef4e9b906aaad760aa71
kimdaehyun <zbeld123@gmail.com>
2021-09-04 16:57:43 +0900

    init

---
+++ src/main/java/com/spingbatchex/SpingBatchExApplication.java
@@ -0,0 +1,13 @@
+package com.spingbatchex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBatchExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBatchExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spingbatchex/SpingBatchExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingbatchex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBatchExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
