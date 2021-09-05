973293ad7fa40de6c9f1440c6d5dcf8e8bc46ea0
kimdaehyun <zbeld123@gmail.com>
2021-09-05 14:14:46 +0900

    task기반 step에서 chunk 구현

--- src/main/java/com/spingbatchex/lab3/ChunkProcessingConfig.java
+++ src/main/java/com/spingbatchex/lab3/ChunkProcessingConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @RequiredArgsConstructor
@@ -38,12 +40,15 @@ public class ChunkProcessingConfig {
     @Bean
     public Step chunkBasedStep() {
         return sbf.get("chunkStep")
+                // <INPUT_TYPE, OUTPUT_TYPE>
+                // chunk(num) : itemWriter 의 OUTPUT_TYPE 리스트의 크기
                 .<String,String>chunk(10)
                 .reader(itemReader())
                 .processor(itemProcessor())
                 .writer(itemWriter())
                 .build();
     }
+
     // 배치 대상 데이터 get
     private ListItemReader itemReader(){
         return new ListItemReader<>(getTestData());
@@ -64,12 +69,31 @@ public class ChunkProcessingConfig {
                 .build();
     }
 
+    /**
+     * tasklet 기반 step에서 chunk 구현
+     */
     private Tasklet tasklet() {
-        return (contribution, chunkContext) ->  {
-            List<String> testData = getTestData();
-            log.info("test data size = {}", testData.size());
 
-            return RepeatStatus.FINISHED;
+        List<String> testData = getTestData();
+
+        return (contribution, chunkContext) -> {
+            StepExecution stepExecution = contribution.getStepExecution();
+
+            int chunkSize = 10;
+            int fromIdx = stepExecution.getReadCount();
+            int toIdx = fromIdx + chunkSize;
+
+            if (fromIdx >= testData.size()) return RepeatStatus.FINISHED;
+
+//            testData.subList(fromIdx, toIdx).stream()
+//                    .map(t -> t + " + batch processing").collect(Collectors.toList());
+//            testData.forEach(log::info);
+
+            log.info("chunk size = {}", testData.subList(fromIdx, toIdx).size());
+
+            stepExecution.setReadCount(toIdx);
+
+            return RepeatStatus.CONTINUABLE;
         };
     }
 
