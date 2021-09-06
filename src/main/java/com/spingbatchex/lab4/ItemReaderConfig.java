72c8459c7aef67c43198c1ea4f99be52eb3ea629
kimdaehyun <zbeld123@gmail.com>
2021-09-06 14:41:37 +0900

    FlatFileItemReader - csv파일 배치 대상 객체로 매핑 (FlatFileItemReader)

--- src/main/java/com/spingbatchex/lab4/ItemReaderConfig.java
+++ src/main/java/com/spingbatchex/lab4/ItemReaderConfig.java
@@ -9,8 +9,14 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +30,11 @@ public class ItemReaderConfig {
     private final StepBuilderFactory sbf;
 
     @Bean
-    public Job itemReaderJob() {
+    public Job itemReaderJob() throws Exception {
         return jbf.get("itemReaderJob")
                 .incrementer(new RunIdIncrementer())
                 .start(customItemReaderStep())
+                .next(csvFileReaderStep())
                 .build();
     }
 
@@ -38,6 +45,45 @@ public class ItemReaderConfig {
                 .reader(new CustomItemReader<>(getItems(10)))
                 .writer(itemWriter())
                 .build();
+    }
+
+    @Bean
+    public Step csvFileReaderStep() throws Exception {
+        return sbf.get("csvFileReaderStep")
+                .<Person, Person>chunk(10)
+                .reader(this.csvFileReader())
+                .writer(this.itemWriter())
+                .build();
+    }
+
+    /**
+     * FlatFileItemReader - csv 파일 read
+     */
+    private FlatFileItemReader<Person> csvFileReader() throws Exception {
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer(); // ' , ' 로 구분
+        lineTokenizer.setNames("id","name","age");
+        lineMapper.setLineTokenizer(lineTokenizer);
+
+        lineMapper.setFieldSetMapper(fieldSet -> {
+            String id = fieldSet.readString("id");
+            String name = fieldSet.readString("name");
+            int age = fieldSet.readInt("age");
+
+            return new Person(id, name, age);
+        });
+
+        FlatFileItemReader<Person> csvFileReader = new FlatFileItemReaderBuilder<Person>()
+                .name("csvFileReader")
+                .encoding("UTF-8")
+                .resource(new ClassPathResource("test/test.csv"))
+                .linesToSkip(1) // 레이블 row 스킵
+                .lineMapper(lineMapper)
+                .build();
+        // ItemReader에 필요한 필수 속성값이 정상적으로 설정되었는지 검증
+        csvFileReader.afterPropertiesSet();
+
+        return csvFileReader;
     }
 
     private ItemWriter<Person> itemWriter() {
--- src/main/java/com/spingbatchex/lab4/Person.java
+++ src/main/java/com/spingbatchex/lab4/Person.java
@@ -9,5 +9,5 @@ public class Person {
 
     private String id;
     private String name;
-    private Integer age;
+    private int age;
 }
